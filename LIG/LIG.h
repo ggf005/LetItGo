1dd05a8c17763c9d1062be60e80fa7a9437e999e
gongguifei <gongguifei@ggftekiMac-mini.local>
2015-02-15 11:36:59 +0800

    update LIG.h

--- LIG/LIG.h
+++ LIG/LIG.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char LIGVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <LIG/PublicHeader.h>
 
 
+
+
