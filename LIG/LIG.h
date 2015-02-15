784683c5c19d99eb830cc55e6ab4fa48d0b69fb6
gongguifei <gongguifei@ggftekiMac-mini.local>
2015-02-15 11:20:32 +0800

    upload LIG project

---
+++ LIG/LIG.h
@@ -0,0 +1,19 @@
+//
+//  LIG.h
+//  LIG
+//
+//  Created by gongguifei on 15/2/15.
+//  Copyright (c) 2015年 Gong Guifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LIG.
+FOUNDATION_EXPORT double LIGVersionNumber;
+
+//! Project version string for LIG.
+FOUNDATION_EXPORT const unsigned char LIGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LIG/PublicHeader.h>
+
+
